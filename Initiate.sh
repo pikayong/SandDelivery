8ce3d920fee800350f46ca68dfee20efc68dce06
Lee Xin Yong <leeleeyong011105@gmail.com>
2024-01-18 20:45:23 +0800

    Add Initiate File

---
+++ Initiate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3
+sudo apt-get install python3-pip
+sudo apt install postgresql postgresql-client
+sudo -u postgres createuser -d -R -S $USER
+createdb $USER
+sudo apt install python3-pip libldap2-dev libpq-dev libsasl2-dev python3-cffi
+pip install -r requirements.txt
+sudo apt install npm
+sudo npm install -g rtlcss
+python3 odoo-bin --addons-path=addons -d mydb
