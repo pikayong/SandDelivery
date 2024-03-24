0ce66088de2f82306d910f9d8cea556955528f50
Lee Xin Yong <leeleeyong011105@gmail.com>
2024-03-25 05:45:57 +0800

    display license plate in trip tree view

--- addons/fleet/models/fleet_vehicle_trip.py
+++ addons/fleet/models/fleet_vehicle_trip.py
@@ -23,6 +23,7 @@ class FleetVehicleTrip(models.Model):
     loading_weight = fields.Float('Weight (tons)', compute='_compute_loading_weight', store=True, readonly=False)
     total = fields.Float('Total (RM)', compute='_compute_total', store=True, readonly=True)
     vehicle_id = fields.Many2one('fleet.vehicle', 'Vehicle', required=True, domain="[('active', '=', True)]")
+    license_plate = fields.Char(related='vehicle_id.license_plate', string="Vehicle", readonly=True, store=False)
     unit = fields.Selection(related='vehicle_id.odometer_unit', string="Unit", readonly=True)
     # driver_id = fields.Many2one(related="vehicle_id.driver_id", string="Driver", readonly=False)
     driver_id = fields.Many2one('res.partner', compute='_compute_default_driver', store=True, string="Driver", readonly=False, required=True, domain="[('active', '=', True)]")
